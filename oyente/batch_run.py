de9cd4e7206cc5188aec8dc9a54eb202fa036ade
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2021-11-29 20:02:11 +0800

    annotation

--- oyente/batch_run.py
+++ oyente/batch_run.py
@@ -1,6 +1,6 @@
 import json
-import glob
-from tqdm import tqdm
+import glob#glob — Unix style pathname pattern expansion
+from tqdm import tqdm#进度条类
 import os
 import sys
 import urllib2
@@ -13,8 +13,8 @@ cjson = {}
 
 print "Loading contracts..."
 
-for cfile in tqdm(cfiles):
-	cjson.update(json.loads(open(cfile).read()))
+for cfile in tqdm(cfiles):#
+	cjson.update(json.loads(open(cfile).read()))#json.loads(open(cfile).read())产生一个字典,键是合约名，值是合约内容
 
 results = {}
 missed = []
@@ -25,26 +25,26 @@ contracts = cjson.keys()
 
 if os.path.isfile('results.json'):
 	old_res = json.loads(open('results.json').read())
-	old_res = old_res.keys()
-	contracts = [c for c in contracts if c not in old_res]
+	old_res = old_res.keys()#旧repository里的合约名
+	contracts = [c for c in contracts if c not in old_res]#挑选不在旧repository里面的合约名，即不重复分析已经分析过的合约
 
 cores=0
 job=0
 
 if len(sys.argv)>=3:
 	cores = int(sys.argv[1])
-	job = int(sys.argv[2])
-	contracts = contracts[(len(contracts)/cores)*job:(len(contracts)/cores)*(job+1)]
+	job = int(sys.argv[2])#分批分析
+	contracts = contracts[(len(contracts)/cores)*job:(len(contracts)/cores)*(job+1)]#要分析的合约列表
 	print "Job %d: Running on %d contracts..." % (job, len(contracts))
 
-for c in tqdm(contracts):
-	with open('tmp.evm','w') as of:
-		of.write(cjson[c][1][2:])
+for c in tqdm(contracts):#c是一个合约名
+	with open('tmp.evm','w') as of:#不知道是什么文件
+		of.write(cjson[c][1][2:])#[2:]是合约的0x后的
 	os.system('python oyente.py -ll 30 -s tmp.evm -j -b')
 	try:
 		results[c] = json.loads(open('tmp.evm.json').read())
 	except:
-		missed.append(c)
+		missed.append(c)#出错记录遗漏的合约
 	with open('results.json', 'w') as of:
 		of.write(json.dumps(results,indent=1))
 	with open('missed.json', 'w') as of:
